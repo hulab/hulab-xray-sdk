1d173e3043da54b7f6097937b1e45b38348fd084
SebC99 <sebastien.caron@gmail.com>
2019-11-06 20:15:13 +0100

    eee

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ const FAKE_XRAY = {
   SegmentUtils: {}
 };
 
-exports.default = parseInt(process.env.AWS_XRAY) ? AWS_XRAY : FAKE_XRAY;
+exports.default = FAKE_XRAY; //parseInt(process.env.AWS_XRAY) ? AWS_XRAY : 
