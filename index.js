a32b6e4f7fdb393b16e8c796bb42357e8565118a
SebC99 <sebastien.caron@gmail.com>
2019-11-06 19:31:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+const AWS_XRAY = require('aws-xray-sdk');
+
+const FAKE_XRAY = {
+  plugins: {},
+  config: () => {},
+  express: {
+    openSegment: () => {},
+    closeSegment: () => {}
+  },
+  captureAWS: (capture) => capture,
+  getSegment: () => {},
+  captureAsyncFunc: (name, callback) => callback(),
+  setAWSWhitelist: () => {},
+  appendAWSWhitelist: () => {},
+  setStreamingThreshold: () => {},
+  setLogger: () => {},
+  getLogger: () => {},
+  setDaemonAddress: () => {},
+  captureFunc: (name, callback) => callback(),
+  captureCallbackFunc: (name, callback) => callback(),
+  captureAWSClient: (client) => client,
+  captureHTTPs: (capture) => capture,
+  captureHTTPsGlobal: (capture) => capture,
+  capturePromise: () => {},
+  utils: {},
+  database: {},
+  middleware: {},
+  getNamespace: () => {},
+  resolveSegment: () => {},
+  setSegment: () => {},
+  isAutomaticMode: () => false,
+  enableAutomaticMode: () => {},
+  enableManualMode: () => {},
+  setContextMissingStrategy: () => {},
+  Segment: {},
+  Subsegment: {},
+  SegmentUtils: {}
+};
+
+exports.module = parseInt(process.env.AWS_XRAY) ? AWS_XRAY : FAKE_XRAY;
